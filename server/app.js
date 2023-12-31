4efc8686c88ece64b9e2129213e3939e99e7c1bc
Andresr35 <andresjruiz@cpp.edu>
2023-12-30 22:22:26 -0800

    set up server

---
+++ server/app.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+
+// Connect to database
+// const mongoose = require("mongoose");
+// mongoose.set("strictQuery", false);
+// const mongoDB = process.env.MONGOPASS;
+// main().catch((err) => console.log(err));
+// async function main() {
+//   await mongoose.connect(mongoDB);
+// }
+
+var indexRouter = require("./routes/index");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(cors({ origin: process.env.WEB }));
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // render the error page
+  res.status(err.status || 500).json({
+    message: "An unknown error has occured",
+    error: err,
+    errorMessage: err.message,
+  });
+});
+
+module.exports = app;
